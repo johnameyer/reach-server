6b26608c7cb8ba7f5688a2ca76c5d54caf06a1ba
fuyi1215 <fuyi1215@msn.com>
2017-02-04 14:24:58 -0500

    Update get.py

--- get.py
+++ get.py
@@ -4,7 +4,7 @@ from flask.ext.pymongo import PyMongo
 app = Flask(__name__)
 mongo = PyMongo(app)
 
-@app.route("/users/<user_id>",methods=['GET'])
+@app.route("/users/<task_id>",methods=['GET'])
 def show(task_id):
     
     #users=mongo.db.reach
