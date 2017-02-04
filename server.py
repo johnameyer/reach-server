6b1f89b95f21340faeee825fae9eef96434cf385
William Markley <willjmarkley@gmail.com>
2017-02-04 15:02:22 -0500

    combined all files

--- server.py
+++ server.py
@@ -1,27 +1,50 @@
 #!/usr/bin/python
 
+## Imports
 from flask import Flask
+from flask import jsonify
+from flask import request
 from flask_pymongo import PyMongo
 import json
 
-app = Flask(__name__)
-mongo = PyMongo(app)
 
+
+## Initialize Flask
+app = Flask('reach')
+mongo = PyMongo(app)
+app.config['MONGO_DBNAME'] = 'reach'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/reach'
+
+
+
+## Routing Functions
 @app.route("/")
 def hello():
     return "Hello World!"
+    
 
-app = Flask('reach')
-mongo = PyMongo(app)
-
-@app.route('/users/<ObjectId:task_id>/', methods=['DELETE'])
-def hello(task_id):
-    #for doc in mongo.db.reach.find({}):
-    #    print(doc)
-    mongo.db.reach.delete_one( { "_id":task_id })
-
-    return "Document Deleted"
+@app.route("/users/<ObjectId:user_id>", methods=['GET'])
+def get_user(user_id):
+    return jsonify(mongo.db.reach.find({"_id":user_id}))
 
 
+@app.route("/users/<ObjectId:user_id>", methods=['POST'])
+def update_user(user_id):
+    users=mongo.db.reach
+    crit={"_id":user_id}
+    dat=request.data.decode("utf-8")
+    users.update_one(crit,{ '$set':json.loads(dat)})
+    return "Updated User"
+
+
+@app.route('/users/<ObjectId:user_id>/', methods=['DELETE'])
+def delete_user(user_id):
+    mongo.db.reach.delete_one( { "_id":user_id })
+    return "Document User"
+
+
+
+## MAIN
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="80")
+
