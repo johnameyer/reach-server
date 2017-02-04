1f959650807e0bbe4d87fd84fcf72e063d42e61e
Jack Meyer <jack10042@gmail.com>
2017-02-04 13:31:12 -0500

    Post updates.

--- post.py
+++ post.py
@@ -1,11 +1,21 @@
 from flask import Flask
-from flask.ext.pymongo import PyMongo
+from flask import jsonify
+from flask import request
+import json
+from flask_pymongo import PyMongo
 
-app = Flask(__name__)
+app = Flask('reach')
 mongo = PyMongo(app)
 
-@app.route("/users/<user_id>",methods=['POST'])
+app.config['MONGO_DBNAME'] = 'reach'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/reach'
+
+@app.route("/users/<ObjectId:user_id>",methods=['POST'])
 def show(user_id):
-    return user_id
+    users=mongo.db.reach
+    crit={"_id":user_id}
+    dat=request.data.decode("utf-8")
+    users.update_one(crit,{ '$set':json.loads(dat)})
+    return "Updated."
 if __name__ == "__main__":
     app.run()
